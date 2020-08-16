5695a47e79ee1104812dead0d13b8e90b6e46cde
Gunjan-Bhatia <69620641+Gunjan-Bhatia@users.noreply.github.com>
2020-08-16 19:21:53 +0530

    Java Assignment Submission Morning Session

---
+++ Question2.java
@@ -0,0 +1,21 @@
+//Question 2. Write a Java program to get a reverse order view of the keys contained in a given map.
+
+import java.util.*;
+
+
+public class Question2 {
+	public static void main(String args[]) {
+
+		
+		TreeMap<String, String> map = new TreeMap<String, String>();
+
+		
+		map.put("C2", "Red");
+		map.put("C1", "Green");
+		map.put("C4", "Black");
+		map.put("C3", "White");
+
+		System.out.println("Orginal content: " + map);
+		System.out.println("Reverse order view of the keys: " + map.descendingKeySet());
+	}
+}
