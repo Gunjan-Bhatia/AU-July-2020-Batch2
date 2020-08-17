9859be9ccd3f7a6ed997bb9cb01c65944ec5e523
Gunjan-Bhatia <69620641+Gunjan-Bhatia@users.noreply.github.com>
2020-08-17 19:09:09 +0530

    Update Question1.java

--- Java-Assignment-Morning-Session/Question1.java
+++ Java-Assignment-Morning-Session/Question1.java
@@ -1,11 +1,10 @@
 //Question 1. Write a Java program to replace each substring of a given string that matches the given regular expression with the given replacement.
-
 import java.util.*;
 
 public class Question1 {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
      Scanner s = new Scanner(System.in);
      System.out.println("Enter String :");
      String str = s.nextLine();
@@ -18,7 +17,20 @@ public class Question1 {
      String result = str.replaceAll(pattern, replacement);
      System.out.println("Result");
      System.out.println(result);
-     
+     s.close();  
 	}
 
 }
+/*
+ Output
+ Enter String :
+Hey Hi
+Enter regular expression :
+H
+
+Enter replacement :
+B
+Result
+Bey Bi
+
+ */
